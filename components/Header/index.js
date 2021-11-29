7718c48d011a48d578fc1ae1f0677a16e807223d
tuvshinbat <tuvshinbat596@gmail.com>
2021-11-30 03:16:39 +0800

    header change

--- components/Header/index.js
+++ components/Header/index.js
@@ -10,41 +10,66 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 
 import { faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const Header = () => {
-  return (
-    <header>
-      <Container>
-        <div className={css.Topbar}>
-          <div className={css.Contacts}>
-            <div className={css.Contact}>
-              <FontAwesomeIcon icon={faPhone} className={css.TopbarIcon} />
-              <a href={`tel:88882470`}> (+976) 8888-2470</a>
-            </div>
-            <div className={css.Contact}>
-              <FontAwesomeIcon icon={faEnvelope} className={css.TopbarIcon} />
-              <a href={`mailto:info@domain.mn`}> info@domain.mn</a>
-            </div>
-          </div>
-          <div className={css.Socials}>
-            <a href="https://www.facebook.com/">
-              <FontAwesomeIcon icon={faFacebookF} className={css.Icons} />
-            </a>
-            <a href="https://www.twitter.com/">
-              <FontAwesomeIcon icon={faTwitter} className={css.Icons} />
-            </a>
-            <a href="https://www.youtube.com/">
-              <FontAwesomeIcon icon={faYoutube} className={css.Icons} />
-            </a>
-          </div>
-        </div>
+  const router = useRouter();
+  useEffect(() => {
+    window.onscroll = () => {
+      let header = document.querySelector(".myHeader");
+      let sticky = header.offsetTop;
 
-        <Navbar expand="lg">
+      if (window.pageYOffset > sticky) {
+        header.classList.add(`${css.Sticky}`);
+        header.classList.add("animate__animated");
+        header.classList.add("animate__slideInDown");
+        header.classList.add("animate__delay-0.2s");
+      } else {
+        header.classList.remove(`${css.Sticky}`);
+        header.classList.remove("animate__animated");
+        header.classList.remove("animate__slideInDown");
+        header.classList.remove("animate__delay-0.2s");
+      }
+    };
+  }, []);
+
+  return (
+    <>
+      <header>
+        <Container>
+          <div className={css.Topbar}>
+            <div className={css.Contacts}>
+              <div className={css.Contact}>
+                <FontAwesomeIcon icon={faPhone} className={css.TopbarIcon} />
+                <a href={`tel:88882470`}> (+976) 8888-2470</a>
+              </div>
+              <div className={css.Contact}>
+                <FontAwesomeIcon icon={faEnvelope} className={css.TopbarIcon} />
+                <a href={`mailto:info@domain.mn`}> info@domain.mn</a>
+              </div>
+            </div>
+            <div className={css.Socials}>
+              <a href="https://www.facebook.com/">
+                <FontAwesomeIcon icon={faFacebookF} className={css.Icons} />
+              </a>
+              <a href="https://www.twitter.com/">
+                <FontAwesomeIcon icon={faTwitter} className={css.Icons} />
+              </a>
+              <a href="https://www.youtube.com/">
+                <FontAwesomeIcon icon={faYoutube} className={css.Icons} />
+              </a>
+            </div>
+          </div>
+        </Container>
+      </header>
+      <Navbar expand="lg" className={`myHeader ${css.Header}`}>
+        <Container>
           <Navbar.Brand href="/">
             {" "}
             <div className={css.Logo}>
               <Image
-                src="/assets/img/logo.png"
+                src="/assets/img/logo-white.png"
                 className={css.LogoImg}
                 layout="fill"
               />
@@ -77,9 +102,9 @@ const Header = () => {
               </ul>
             </Nav>
           </Navbar.Collapse>
-        </Navbar>
-      </Container>
-    </header>
+        </Container>
+      </Navbar>
+    </>
   );
 };
 
